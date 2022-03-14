3815306e66b6f598ca44fb461e0dee1d5e3a16a7
Alexander Lukyanov <lux-nova@mail.ru>
2022-03-14 13:46:59 +0300

    Initial commit

---
+++ src/main/java/ru/netology/springdb/SpringDbApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.springdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbApplication.class, args);
+	}
+
+}

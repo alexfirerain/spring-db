0b69492ae563242ca3ed2bc106591ae1caa418ce
Alexander Lukyanov <lux-nova@mail.ru>
2022-03-22 22:07:51 +0300

    +table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,13 @@ values ('Ame', 'Sury', 19, '+93789'),
        ('Dame', 'Suru', 26, '+94483'),
        ('Fame', 'Sure', 25, '+93156');
 
+insert into orders
+    (date, customer_id, product_name, amount)
+values ('05-05-1998', 3, 'stuff', 8),
+       ('12-12-2003', 1, 'staff', 6),
+       ('15-08-2017', 5, 'buff', 2),
+       ('04-08-2015', 4, 'guff', 5),
+       ('06-12-2014', 1, 'duff', 9),
+       ('09-12-2014', 5, 'cuff', 12),
+       ('05-09-2008', 4, 'laugh', 6),
+       ('09-04-2000', 2, 'dove', 8);
